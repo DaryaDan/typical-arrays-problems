95715cf7cd7101ff3661e422387ada6dce31fd75
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-03-17 00:04:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){return 0;}
+return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length == 0){return 0;}
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length == 0){return 0;}
+  var x, sum = 0;
+      for (x = 0; x < array.length; x++) {
+              sum += array[x];
+      }
+      return (sum/array.length);
 }
